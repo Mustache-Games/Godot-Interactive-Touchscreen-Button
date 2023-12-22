bd2730facd0298b7e7d43c6c88e919156ab0280a
mustachedev0@gmail.com <mustachedev0@gmail.com>
2023-12-22 20:53:46 +0200

    Godot 4 port

--- interactive_touchscreen_button.gd
+++ interactive_touchscreen_button.gd
@@ -1,62 +1,62 @@
-class_name InteractiveTouchscreenButton,"res://Interactive Touchscreen Button.svg"
+@icon("res://Interactive Touchscreen Button.svg")
+class_name InteractiveTouchscreenButton
 extends TextureButton
 
-const default_values := {
+const DefaultValues := {
 	"expand" : true,
+	"ignore_texture_size" : true,
 	"stretch_mode" : TextureButton.STRETCH_KEEP_ASPECT_CENTERED,
 	"action_mode" : TextureButton.ACTION_MODE_BUTTON_PRESS,
 	"focus_mode" : TextureButton.FOCUS_NONE,
 }
 
-export var input_action:String
-export var use_default_values := true
-export var touchscreen_only := false
+@export var input_action:StringName
+@export var use_default_values := true
+@export var touchscreem_only := false
 
 var touch_index := 0
 var released := true
 
-func _init() -> void :
+func _init():
 	if use_default_values :
-		for p in default_values.keys() :
-			set(p, default_values.get(p))
-	if touchscreen_only and not OS.has_touchscreen_ui_hint() :
+		for k in DefaultValues.keys() :
+			self.set(k, DefaultValues.get(k))
+	
+	if touchscreem_only and not DisplayServer.is_touchscreen_available() :
 		hide()
 
 
-func press() -> void :
-	var action = InputEventAction.new()
-	action.action = input_action
-	action.pressed = true
-	Input.parse_input_event(action)
+func press():
+	var event = InputEventAction.new()
+	event.action = input_action
+	event.pressed = true
+	Input.parse_input_event(event)
 	released = false
 
 
-func release() -> void :
-	var action = InputEventAction.new()
-	action.action = input_action
-	action.pressed = false
-	Input.parse_input_event(action)
+func release():
+	var event = InputEventAction.new()
+	event.action = input_action
+	event.pressed = false
+	Input.parse_input_event(event)
 	released = true
 
 
-func is_inside(pos:Vector2) -> bool :
-	if int(pos.x) in range(rect_position.x, rect_position.x+(rect_size.x*rect_scale.x)) :
-		if int(pos.y) in range(rect_position.y, rect_position.y+(rect_size.y*rect_scale.y)) :
+func is_in(pos:Vector2) -> bool:
+	if int(pos.x) in range(position.x, position.x+size.x) :
+		if int(pos.y) in range(position.y, position.y+size.y) :
 			return true
 	return false
 
 
-func _input(event) :
+func _input(event):
 	if event is InputEventScreenTouch :
-		if is_visible_in_tree() and is_in(event.position) :
-			if event.pressed :
-				if released :
-					touch_index = event.index
-				if touch_index == event.index :
-					press()
-				else :
-					release()
+		if event.pressed and is_in(event.position) :
+			if released :
+				touch_index = event.index
+			if touch_index == event.index :
+				press()
 			else :
 				release()
-		if event.index == touch_index and not event.pressed :
+		if touch_index == event.index and not event.pressed :
 			release()
